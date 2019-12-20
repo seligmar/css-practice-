fb63abe20a28e08eca3afa504438292b720bd05e
github username <github email address>
2019-12-20 14:04:30 -0500

    made trash panda spin

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var h3 = document.querySelector('#root')
 h3.addEventListener('click', e => {
   if (h3.id === 'root') {
     h3.id = 'rootsToo'
-    h3.innerHTML = `<p>It no longer spins!</p>`
-  }
-  // &&
-  else {
+    h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
+  } else {
     h3.id = 'root'
-    h3.innerText = 'It spins!'
+    h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
   }
 })
