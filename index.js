bfbed69b0dcbcd7ed97d9fa39725aaa3fbe1f115
github username <github email address>
2019-12-26 13:16:56 -0500

    changed code to flip coin; inspired by 'coin flip' code pen by Le Liu https://codepen.io/le0864/pen/pbmoVQ

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var h3 = document.querySelector('#root')
+var h3 = document.querySelector('#coin')
 
-h3.addEventListener('click', e => {
-  if (h3.id === 'root') {
-    h3.id = 'rootsToo'
-    h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
+h3.addEventListener('click', () => {
+  if (h3.className === 'heads') {
+    h3.className = 'tails'
+    // h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
   } else {
-    h3.id = 'root'
-    h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
+    h3.className = 'heads'
+    // h3.innerHTML = `<iframe src="https://giphy.com/embed/sYmqnfrpu6NCU" width="480" height="317" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
   }
 })
